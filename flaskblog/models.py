b543a6bdc2d33ca87a15d789150f4945f1d3359c
Shaan <shanmuhammed789@gmail.com>
2025-09-22 13:56:14 +0530

    working on adding comments to post

--- flaskblog/models.py
+++ flaskblog/models.py
@@ -9,23 +9,46 @@ def load_user(user_id):
 
 
 class User(db.Model, UserMixin):
+
+    __tablename__ = 'user'
+
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
-    posts = db.relationship('Post', backref='author', lazy=True)
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
 
 
 class Post(db.Model):
+
+    __tablename__ = 'post'
+
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     content = db.Column(db.Text, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
     def __repr__(self):
         return f"Post('{self.title}', '{self.date_posted}')"
+
+
+class Review(db.Model):
+
+    __tablename__ = 'review'
+
+    id = db.Column(db.Integer, primary_key=True)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id'), nullable=False)
+
+    post = db.relationship('Post', lazy=True)
+    user = db.relationship('User', lazy=True)
+
+    def __repr__(self):
+        return f"Review('user : {self.user_id}', 'post : {self.post_id}\nreview : {self.content}')"
+    
+    
--- temp.py
+++
@@ -1,3 +0,0 @@
-from flaskblog import db, app  # replace with your actual app/db names
-with app.app_context():
-    db.create_all()
