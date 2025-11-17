df279691b2c89c0b404a21056dbfb10ba92bdf88
Shaan <shanmuhammed789@gmail.com>
2025-11-17 18:06:35 +0530

    minor changes

--- data_refresh.py
+++ data_refresh.py
@@ -2,12 +2,11 @@ from data_loader import run_import_cycle
 from data_scrapper import run_scrapper
 import time
 
-hrs_to_sleep = 1
+hours_to_sleep = 1
 while(True):
     run_scrapper()
     run_import_cycle()
     print("*"*50)
     print("Sleeping 1 hour till next refresh")
     print("*"*50)
-    # time.sleep(hrs_to_sleep*3600)
-    time.sleep(3)
+    time.sleep(hours_to_sleep*3600)
