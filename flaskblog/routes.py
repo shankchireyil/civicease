3887e92c6cfb237e824a8c5a743eac3dc2313337
nithinreddy <25m0752@iitb.ac.in>
2025-11-23 15:54:00 +0530

    minor changes

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -6,7 +6,6 @@ from flask import render_template, url_for, flash, redirect, request
 from flaskblog import app, db, bcrypt
 from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
 from flaskblog.models import User, Post, Review, Interest, Notification
-from flaskblog.email_reminders import check_and_send_reminders
 from flask_login import login_user, current_user, logout_user, login_required
 from sqlalchemy import func, desc
 from datetime import datetime
